ef9c4da66f04b4e8733e9b1be49767f8334901bc
AdamDagi <mugutdinovshiha02@gmail.com>
2021-10-15 20:27:13 -0400

    wrote firs js code and main var and const tags, devidet for prompt structures.

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,26 @@ function writePassword() {
 
   passwordText.value = password;
 
+};
+
+const typeOperation = ["getLength", "chooseSpecialCharacters"];
+var lengthPassword;
+var chooseSpecialCharacters;
+
+for (var index in typeOperation) {
+  if (typeOperation[index] == "getLength") {
+    const lengthPasswordStr = prompt("How many characters do you want? No less than 8 characters and no more than 128.");
+    if (Number(lengthPasswordStr) > 8 && Number(lengthPasswordStr) < 128) {
+      lengthPassword = Number(lengthPasswordStr);
+    }
+  }
+  if (typeOperation[index] == "chooseSpecialCharacters") {
+    const chooseSpecialCharactersStr = prompt("Please enter at least one special character for password. Lowercase, uppercase, numeric, and/or special characters");
+    const arrayCriterias = chooseSpecialCharactersStr.split(",");
+    
+  }
+
+//  console.log(typeOperation[index]);
 }
 
 // Add event listener to generate button
